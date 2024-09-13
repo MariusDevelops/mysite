4bbcb73907f9dee9e10218615f8c570a8b967c23
MariusDevelops <brazasmarius@gmail.com>
2024-09-13 15:54:07 +0300

    init: project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="bg-black-100 relative mx-auto flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10 ">
+      <div className="w-full max-w-7xl">
+        <h1>Portfolio</h1>
+      </div>
+    </main>
+  );
+}
