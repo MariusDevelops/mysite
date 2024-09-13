986f24e9fdd06bea75e49ccc1a79d39f43b239c9
MariusDevelops <brazasmarius@gmail.com>
2024-09-14 02:55:35 +0300

    feat: add hero text generate effect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
@@ -35,6 +36,28 @@ const Hero = () => {
           className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white
          [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"
         />
+      </div>
+
+      <div className="relative z-10 my-20 flex justify-center">
+        <div className="flex max-w-[89vw] flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
+          <p className="max-w-80 text-center text-xs uppercase tracking-widest text-blue-100">
+            Dynamic Web Magic with Next.js
+          </p>
+
+          {/**
+           *  Link: https://ui.aceternity.com/components/text-generate-effect
+           *
+           *  change md:text-6xl, add more responsive code
+           */}
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="mb-4 text-center text-sm md:text-lg md:tracking-wider lg:text-2xl">
+            Hi! I&apos;m Marius, a Next.js Developer based in Lithuania.
+          </p>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,10 +20,18 @@ const config: Config = {
         black: {
           DEFAULT: "#000",
           100: "#000319",
+          200: "rgba(17, 25, 40, 0.75)",
+          300: "rgba(255, 255, 255, 0.125)",
         },
         white: {
           DEFAULT: "#FFF",
+          100: "#BEC1DD",
+          200: "#C1C2D3",
         },
+        blue: {
+          "100": "#E4ECFF",
+        },
+        purple: "#CBACF9",
       },
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
