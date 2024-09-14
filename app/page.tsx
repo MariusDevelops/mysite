2a1ee58d97a4a10ec629165b62d4584ba355fb4a
MariusDevelops <brazasmarius@gmail.com>
2024-09-14 03:41:46 +0300

    feat: floating navbar setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="bg-black-100 relative mx-auto flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
         <Hero />
+        <FloatingNav navItems={navItems} />
       </div>
     </main>
   );
